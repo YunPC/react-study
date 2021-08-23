33d53dbe677b2530beb63885d79467d3be6112bb
YunPC <apsn2000@nate.com>
2021-08-23 20:41:43 +0900

    Hello 컴포넌트로부터 props를 받아 콘솔창에 출력(#4)

--- src/App.js
+++ src/App.js
@@ -3,20 +3,8 @@ import Hello from './Hello'
 import './App.css'
 
 function App() {
-  const name = 'react'
-  const style = {
-    backgroundColor: 'black',
-    color: 'aqua',
-    fontSize: 24,
-    padding: '1rem'
-  }
   return (
-    <>
-      {/* This is Comment */}
-      <Hello></Hello>
-      <div style={style}>{name}</div>
-      <div className="gray-box"></div>
-    </>
+    <Hello name="react"/>
   );
 }
 
