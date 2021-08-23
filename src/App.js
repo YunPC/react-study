35256978bfc22666c6a46422a285ea00090c04a2
YunPC <apsn2000@nate.com>
2021-08-23 20:28:09 +0900

    feat: Adjust inline style in div In JSX, make style object to adjust inline style

--- src/App.js
+++ src/App.js
@@ -3,10 +3,16 @@ import Hello from './Hello'
 
 function App() {
   const name = 'react'
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: 24,
+    padding: '1rem'
+  }
   return (
     <>
       <Hello></Hello>
-      <div>{name}</div>
+      <div style={style}>{name}</div>
     </>
   );
 }
