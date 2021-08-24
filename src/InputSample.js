83d3b7ec265615f1ed4d1cf60c085e5ef59c192a
YunPC <apsn2000@nate.com>
2021-08-24 20:40:42 +0900

    여러개의 input 상태 관리(#8)

--- src/InputSample.js
+++ src/InputSample.js
@@ -1,20 +1,18 @@
 import React, {useState} from 'react';
 
 function InputSample() {
-    const [text, setText] = useState('');
     const onChange = (e) => {
-        setText(e.target.value);
     }
     const onReset = () => {
-        setText('');
     }
     return (
         <div>
-            <input onChange={onChange} value={text}/>
-            <button>초기화</button>
+            <input placeholder="이름"/>
+            <input placeholder="닉네임"/>
+            <button onClick={onReset}>초기화</button>
             <div>
                 <b>값: </b>
-                {text}
+                이름 닉네임
             </div>
         </div>
     )
