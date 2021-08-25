bd427c8f133f587a9eae0701be021150f8420c26
YunPC <apsn2000@nate.com>
2021-08-25 12:29:55 +0900

    삭제 버튼의 클릭 이벤트에 함수 바인딩하기

--- src/UserList.js
+++ src/UserList.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 function User({user, onRemove}){
+  const {useranme, email, id} = user;
   return(
     <div>
-        <b>{user.username}</b> <span>{user.email}</span>
-        <button>삭제</button>
+        <b>{username}</b> <span>{email}</span>
+        <button onClick={() => onRemove(id)}>삭제</button>
       </div>
   );
 }
