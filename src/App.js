5ae163b142711d17e0a1e16543e6491069bda586
YunPC <apsn2000@nate.com>
2021-08-25 11:15:30 +0900

    CreateUser에 전달할 props 설정하기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React, {useRef} from 'react';
+import React, {useRef, useState} from 'react';
 import './App.css'
 import CreateUser from './CreateUser';
 import UserList from './UserList';
 
 function App() {
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: '',
+  })
+
+  const {username, email} = inputs;
+  const onChange = e => {
+    const {name, value} = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    })
+  }
+
   const users = [
     {
       id: 1,
@@ -25,13 +39,20 @@ function App() {
   const nextId = useRef(4);
 
   const onCreate = () => {
+    setInputs({
+      username: '',
+      email: ''
+    })
     console.log(nextId.current) //4
     nextId.current += 1;
   }
 
   return (
     <>
-    <CreateUser/>
+    <CreateUser username = {username}
+    email={email}
+    onChange={onChange}
+    onCreate={onCreate}/>
     <UserList users={users}/>
     </>
   );
