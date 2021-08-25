66bbb24c7847c5d43123009b7a96236aaa5f44b5
YunPC <apsn2000@nate.com>
2021-08-25 12:47:53 +0900

    onToggle을 만들어 active의 값을 토글한다.(#14)

--- src/App.js
+++ src/App.js
@@ -22,17 +22,20 @@ function App() {
     {
       id: 1,
       username: 'velopert',
-      email: 'public.velopert@gmail.com'
+      email: 'public.velopert@gmail.com',
+      active: true
     },
     {
       id: 2,
       username: 'tester',
-      email: 'tester@example.com'
+      email: 'tester@example.com',
+      active: false
     },
     {
       id: 3,
       username: 'liz',
-      email: 'lix@example.com'
+      email: 'lix@example.com',
+      active: false
     }
   ]);
 
@@ -57,13 +60,17 @@ function App() {
     setUsers(users.filter(user => user.id !== id));
   }
 
+  const onToggle = id => {
+    setUsers(users.map(user => user.id === id ? {...user, active: !user.active} : user))
+  }
+
   return (
     <>
     <CreateUser username = {username}
     email={email}
     onChange={onChange}
     onCreate={onCreate}/>
-    <UserList users={users} onRemove={onRemove}/>
+    <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
     </>
   );
 }
