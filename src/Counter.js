461aa42f6cb404865f7e375188cd47c0743cadab
YunPC <apsn2000@nate.com>
2021-08-30 16:44:55 +0900

    dispatch를 통해 action 전달하기(#19)

--- src/Counter.js
+++ src/Counter.js
@@ -1,7 +1,7 @@
 import React, { useReducer } from "react";
 
-function reducer(state, action){
-  switch (action.type){
+function reducer(state, action) {
+  switch (action.type) {
     case 'INCREMENT':
       return state + 1;
     case 'DECREMENT':
@@ -13,20 +13,26 @@ function reducer(state, action){
 
 function Counter() {
 
-    const onIncrease = () => {
-        setNumber(prevNumber => prevNumber + 1)
-    }
-    const onDecrease = () => {
-        setNumber(prevNumber => prevNumber - 1)
-    }
+  const [number, dispatch] = useReducer(reducer, 0);
 
-    return (
-        <div>
-            <h1>{number}</h1>
-            <button onClick={onIncrease}>+1</button>
-            <button onClick={onDecrease}>-1</button>
-        </div>
-    )
+  const onIncrease = () => {
+    dispatch({
+      type: 'INCREMENT'
+    })
+  }
+  const onDecrease = () => {
+    dispatch({
+      type: 'DECREMENT'
+    })
+  }
+
+  return (
+    <div>
+      <h1>{number}</h1>
+      <button onClick={onIncrease}>+1</button>
+      <button onClick={onDecrease}>-1</button>
+    </div>
+  )
 }
 
 export default Counter;
