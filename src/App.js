a452465b9e157803bd997625ec0d84fa7b7dbbe0
YunPC <apsn2000@nate.com>
2021-08-30 17:19:08 +0900

    onToggle, onRemove 작성 & UserList에 props로 넘겨주기(#19)

--- src/App.js
+++ src/App.js
@@ -51,6 +51,17 @@ function reducer(state, action) {
         inputs: initialState.inputs,
         users: state.users.concat(action.user)
       }
+    case 'TOGGLE_USER':
+      return {
+        ...state,
+        users: state.users.map(user=> 
+          user.id === action.id ? {...user, active: !user.active} : user)
+      }
+    case 'REMOVE_USER':
+      return {
+        ...state,
+        users: state.users.filter(user => user.id !== action.id)
+      }
     default:
       throw new Error('Unhandled action');
   }
@@ -83,10 +94,24 @@ function App() {
     nextId.current += 1;
   }, [username, email])
 
+  const onToggle = useCallback(id => {
+    dispatch({
+      type: 'TOGGLE_USER',
+      id
+    })
+  }, []);
+
+  const onRemove = useCallback(id => {
+    dispatch({
+      type: 'REMOVE_USER',
+      id
+    })
+  }, [])
+
   return (
     <>
     <CreateUser username={username} eamil={email} onChange={onChange} onCreate={onCreate}/>
-    <UserList users={users}/>
+    <UserList users={users} onToggle={onToggle} onRemove={onRemove}/>
     <div>활성 사용자수 : 0</div>
     </>
   );
