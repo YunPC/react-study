c88b286944b730cdbfffd55bf8ea703b688e3ff8
YunPC <apsn2000@nate.com>
2021-08-30 21:03:28 +0900

    useState를 활용하여 유동적으로 값을 전달하기(#21)

--- src/ContextSample.js
+++ src/ContextSample.js
@@ -1,4 +1,4 @@
-import React, {createContext, useContext} from 'react';
+import React, {createContext, useContext, useState} from 'react';
 
 const MyContext = createContext('defaultValue');
 
@@ -14,9 +14,11 @@ function GrandParent(){
   return <Parent />
 }
 function ContextSample(){
+  const [value, setValue] = useState(true);
   return (
-    <MyContext.Provider value="GOOD">
+    <MyContext.Provider value={value ? 'GOOD' : 'BAD'}>
       <GrandParent />
+      <button onClick={() => setValue(!value)}>CLICK ME</button>
     </MyContext.Provider>
   )
 }
