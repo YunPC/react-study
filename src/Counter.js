5503ee2debbc0b147bbcd28c685cfd163950aaeb
YunPC <apsn2000@nate.com>
2021-08-31 14:07:17 +0900

    constructor를 활용하여 Counter 상태 관리(#23)

--- src/Counter.js
+++ src/Counter.js
@@ -2,18 +2,29 @@ import React, { useReducer } from 'react'
 
 class Counter extends React.Component {
 
+  constructor(props){
+    super(props);
+    this.state = {
+      counter: 0
+    }
+  }
+
   handleIncrease = () => {
-    console.log('increase');
+    this.setState({
+      counter: this.state.counter + 1
+    })
   }
 
   handleDecrease = () => {
-    console.log('decrease');
+    this.setState({
+      counter: this.state.counter - 1
+    })
   }
 
   render() {
     return (
       <div>
-        <h1>0</h1>
+        <h1>{this.state.counter}</h1>
         <button onClick={this.handleIncrease}>+1</button>
         <button onClick={this.handleDecrease}>-1</button>
       </div>
