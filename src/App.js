0fdb84874a26906fab4d55d1e07aabcf8ad527fb
YunPC <apsn2000@nate.com>
2021-08-31 13:33:26 +0900

    reducer를 immer를 사용하여 구현(#22)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { useRef, useReducer, useMemo, useCallback, createContext } from 'react'
+import produce from 'immer'
 import CreateUser from './CreateUser'
 import UserList from './UserList'
 import useInputs from './useInputs'
+import { unstable_wrapCallback } from 'scheduler'
 
 function countActiveUsers(users) {
   console.log('활성 사용자 세는중...')
@@ -34,21 +36,32 @@ const initialState = {
 function reducer(state, action) {
   switch (action.type) {
     case 'CREATE_USER':
-      return {
-        users: state.users.concat(action.user),
-      }
+      return produce(state, draft => {
+        draft.users.push(action.user)
+      })
+      // return {
+      //   users: state.users.concat(action.user),
+      // }
     case 'TOGGLE_USER':
-      return {
-        ...state,
-        users: state.users.map((user) =>
-          user.id === action.id ? { ...user, active: !user.active } : user
-        ),
-      }
+      return produce(state, draft => {
+        const user = draft.users.find(user => user.id === action.id);
+        user.active = !user.active;
+      })
+      // return {
+      //   ...state,
+      //   users: state.users.map((user) =>
+      //     user.id === action.id ? { ...user, active: !user.active } : user
+      //   ),
+      // }
     case 'REMOVE_USER':
-      return {
-        ...state,
-        users: state.users.filter((user) => user.id !== action.id),
-      }
+      return produce(state, draft => {
+        const index = draft.users.findIndex(user => user.id === action.id);
+        draft.users.splice(index, 1);
+      })
+      // return {
+      //   ...state,
+      //   users: state.users.filter((user) => user.id !== action.id),
+      // }
     default:
       throw new Error('Unhandled action')
   }
