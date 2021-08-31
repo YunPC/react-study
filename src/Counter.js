8ced1312d5c917bc0dd145590d1176dbac8064dc
YunPC <apsn2000@nate.com>
2021-08-31 14:08:35 +0900

    클래스 프로퍼티를 통해 상태관리(#23)

--- src/Counter.js
+++ src/Counter.js
@@ -2,11 +2,8 @@ import React, { useReducer } from 'react'
 
 class Counter extends React.Component {
 
-  constructor(props){
-    super(props);
-    this.state = {
-      counter: 0
-    }
+  state = {
+    counter: 0
   }
 
   handleIncrease = () => {
