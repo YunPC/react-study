03af249f07c5b0c1fed8277dd4ec1b71ff570e81
YunPC <apsn2000@nate.com>
2021-08-31 14:02:41 +0900

    constructor 내부에서 this를 바인딩하기(#23)

--- src/Counter.js
+++ src/Counter.js
@@ -1,12 +1,27 @@
 import React, { useReducer } from 'react'
 
 class Counter extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.handleIncrease = this.handleIncrease.bind(this);
+    this.handleDecrease = this.handleDecrease.bind(this);
+  }
+
+  handleIncrease(){
+    console.log('increase');
+  }
+
+  handleDecrease(){
+    console.log('decrease');
+  }
+
   render() {
     return (
       <div>
         <h1>0</h1>
-        <button>+1</button>
-        <button>-1</button>
+        <button onClick={this.handleIncrease}>+1</button>
+        <button onClick={this.handleDecrease}>-1</button>
       </div>
     )
   }
